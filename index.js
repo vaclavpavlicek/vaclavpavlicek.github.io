0343a4da6c5bf5689c209b402a7abd2cdb7be20e
Václav Pavlíček <vaclav@pavlicek.cz>
2023-06-15 21:44:42 +0100

    fix mobile menu

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let menuOpened = false;
+
 window.scrollToAbout = function() {
     const aboutSection = document.getElementById("about-container");
     window.scrollTo({
@@ -15,16 +17,40 @@ window.scrollToSection = function(id) {
 };
 
 window.openMenu = function() {
-    console.log('hello');
+    menuOpened = !menuOpened;
     const menuItemsElement = document.getElementById('js-mobile-menu-items');
     menuItemsElement.classList.toggle('visible');
     const menuButton = document.getElementById('js-menu-button');
     menuButton.classList.toggle('closed-menu');
+    console
 };
 
+window.mainLinkClicked = function() {
+    console.log("menuOpened");
+    console.log(menuOpened);
+    if (menuOpened) {
+        const menuButton = document.getElementById('js-menu-button');
+        menuButton.classList.toggle('closed-menu');
+        const menuItemsElement = document.getElementById('js-mobile-menu-items');
+        menuItemsElement.classList.toggle('visible');
+        menuOpened = false;
+        window.scrollTo({
+            top: 0
+        });
+    } else {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+}
+
 window.menuItemClicked = function(id) {
+    menuOpened = !menuOpened;
     const menuItemsElement = document.getElementById('js-mobile-menu-items');
     menuItemsElement.classList.toggle('visible');
+    const menuButton = document.getElementById('js-menu-button');
+    menuButton.classList.toggle('closed-menu');
     const section = document.getElementById(id);
     window.scrollTo({
         top: section.offsetTop - 48,
