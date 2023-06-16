5e6409aba0363657e1da0c0f1a6a2cf1dfafb2d9
Václav Pavlíček <vaclav@pavlicek.cz>
2023-06-16 12:19:34 +0100

    grammar fixes

--- index.js
+++ index.js
@@ -1,69 +1,63 @@
-let menuOpened = false;
-
-window.scrollToAbout = function() {
-    const aboutSection = document.getElementById("about-container");
-    window.scrollTo({
-        top: aboutSection.offsetTop,
-        behavior: 'smooth'
-    });
+var menuOpened = false;
+window.scrollToAbout = function () {
+  var aboutSection = document.getElementById("about-container");
+  window.scrollTo({
+    top: aboutSection.offsetTop,
+    behavior: 'smooth'
+  });
 };
-
-window.scrollToSection = function(id) {
-    const section = document.getElementById(id);
-    window.scrollTo({
-        top: section.offsetTop - 48,
-        behavior: 'smooth'
-    });
+window.scrollToSection = function (id) {
+  var section = document.getElementById(id);
+  window.scrollTo({
+    top: section.offsetTop - 48,
+    behavior: 'smooth'
+  });
 };
-
-window.openMenu = function() {
-    menuOpened = !menuOpened;
-    const menuItemsElement = document.getElementById('js-mobile-menu-items');
-    menuItemsElement.classList.toggle('visible');
-    const menuButton = document.getElementById('js-menu-button');
+window.openMenu = function () {
+  menuOpened = !menuOpened;
+  var menuItemsElement = document.getElementById('js-mobile-menu-items');
+  menuItemsElement.classList.toggle('visible');
+  var menuButton = document.getElementById('js-menu-button');
+  menuButton.classList.toggle('closed-menu');
+  updateBodyOverflow();
+};
+window.mainLinkClicked = function () {
+  if (menuOpened) {
+    var menuButton = document.getElementById('js-menu-button');
     menuButton.classList.toggle('closed-menu');
-    updateBodyOverflow();
+    var menuItemsElement = document.getElementById('js-mobile-menu-items');
+    menuItemsElement.classList.toggle('visible');
+    menuOpened = false;
+    window.scrollTo({
+      top: 0
+    });
+  } else {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  updateBodyOverflow();
 };
-
-window.mainLinkClicked = function() {
+window.menuItemClicked = function (id) {
+  menuOpened = !menuOpened;
+  var menuItemsElement = document.getElementById('js-mobile-menu-items');
+  menuItemsElement.classList.toggle('visible');
+  var menuButton = document.getElementById('js-menu-button');
+  menuButton.classList.toggle('closed-menu');
+  var section = document.getElementById(id);
+  window.scrollTo({
+    top: section.offsetTop - 48
+  });
+  updateBodyOverflow();
+};
+window.updateBodyOverflow = function () {
+  var bodyElements = document.getElementsByTagName("body");
+  if (bodyElements.length > 0) {
     if (menuOpened) {
-        const menuButton = document.getElementById('js-menu-button');
-        menuButton.classList.toggle('closed-menu');
-        const menuItemsElement = document.getElementById('js-mobile-menu-items');
-        menuItemsElement.classList.toggle('visible');
-        menuOpened = false;
-        window.scrollTo({
-            top: 0
-        });
+      bodyElements[0].style.overflow = "hidden";
     } else {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
+      bodyElements[0].style.overflow = "";
     }
-    updateBodyOverflow();
-}
-
-window.menuItemClicked = function(id) {
-    menuOpened = !menuOpened;
-    const menuItemsElement = document.getElementById('js-mobile-menu-items');
-    menuItemsElement.classList.toggle('visible');
-    const menuButton = document.getElementById('js-menu-button');
-    menuButton.classList.toggle('closed-menu');
-    const section = document.getElementById(id);
-    window.scrollTo({
-        top: section.offsetTop - 48,
-    });
-    updateBodyOverflow();
-}
-
-window.updateBodyOverflow = function() {
-    const bodyElements = document.getElementsByTagName("body");
-    if (bodyElements.length > 0) {
-        if (menuOpened) {
-            bodyElements[0].style.overflow = "hidden";
-        } else {
-            bodyElements[0].style.overflow = "";
-        }
-    }
-}
+  }
+};
